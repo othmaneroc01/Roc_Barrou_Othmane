b8e9692560d4f791e9f79ce9ef64469cb9195b6e
Table4 <touloniut@gmail.com>
2023-03-28 16:35:05 +0200

    28/03 pp

---
+++ C#/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ExtendedSerialPort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ReliableSerialPort serialPort1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM4", 115200, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+            serialPort1.OnDataReceivedEvent += SerialPort1_OnDataReceivedEvent;
+            serialPort1.Open();
+        }
+
+        private void SerialPort1_OnDataReceivedEvent(object sender, DataReceivedArgs e)
+        {
+            //textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
+        int n = 0;
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        void SendMessage()
+        {
+            serialPort1.WriteLine(textBoxEmission.Text);
+            textBoxReception.Text += "Recu : " + textBoxEmission.Text + "\n";
+            textBoxEmission.Text = "";
+        }
+    }
+}
